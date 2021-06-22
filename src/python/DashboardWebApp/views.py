3db3bef59f365f9a65b21541041b94ff592625f8
nerdboyq <princtonb@gmail.com>
2021-06-22 08:10:46 -0700

    added methods to handle updating and retrieving new behavior record model entries

--- src/python/DashboardWebApp/views.py
+++ src/python/DashboardWebApp/views.py
@@ -4,6 +4,36 @@ from DashboardWebApp.models import *
 from flask_sqlalchemy import SQLAlchemy
 
 
+def get_behavior_records():
+	"""
+	Returns list of behavior records from db.
+	"""
+	return BehaviorRecord.query.all()
+
+
+def get_behavior_record_values(date: str):
+	"""
+	Gets Behavior Record values for a specific date.
+	"""
+	try:
+		rec = BehaviorRecord.query.filter_by(date=date).first()
+
+		# Catch nonexistent record for specified date and create one.
+		if rec is None:
+			print(f"-i- Adding nnew record for {date}")
+			rec = BehaviorRecord(happy_point_count=0, sad_point_count=0, bonus_point_count=0)
+			db.session.add(rec)
+			db.session.commit()
+
+		print(f"-i- {rec}")
+		return rec
+
+	except Exception as e:
+		print(f'Error finding behavior records: {e}')
+
+		return None
+
+
 @app.route('/get_alarms', methods=['GET'])
 def get_alarms_from_db():
 	"""
@@ -46,11 +76,41 @@ def remove_alarm(alarms: list):
 			Alarm.query.filter_by(name=alarm_name).delete()
 	
 
+@app.route("/update_behavior_points/<update_opt>", methods=['GET', 'POST'])
+def update_behavior_points(update_opt):
+	"""
+	Updates happy, sad, or bonus points for the current date behavior record.
+
+	@param update_opt: update option
+	@type update_opt: str
+	"""
+	rec: BehaviorRecord
+	rec = get_behavior_record_values(get_date_string())
+	if update_opt != "-1":
+		if update_opt == "0":
+			rec.happy_point_count += 1
+		elif update_opt == "1" and rec.happy_point_count > 0:
+			rec.happy_point_count -= 1
+		elif update_opt == "2":
+			rec.sad_point_count += 1
+		elif update_opt == "3" and rec.sad_point_count > 0:
+			rec.sad_point_count -= 1
+		elif update_opt == "4":
+			rec.bonus_point_count += 1
+		elif update_opt == "5" and rec.bonus_point_count > 0:
+			rec.bonus_point_count -= 1
+
+		db.session.commit()
+
+	return jsonify({"status": "success", "behavior_record": rec.payload})
+
+
 @app.route("/")
 def render_homepage():
 	"""
 	Starts homepage for the webapp.
 	"""
+
 	return render_template('homepage.html', alarms=Alarm.query.all())
 	
 
